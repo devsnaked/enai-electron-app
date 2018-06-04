81c639e1f205ff82525a9c89e74aa643389933c0
Leonardo Victor <leonardovff@gmail.com>
2018-06-04 18:07:57 -0300

    correct

--- react/src/components/qr-reader/qrReader.js
+++ react/src/components/qr-reader/qrReader.js
@@ -6,8 +6,8 @@ import { Link } from 'react-router-dom';
 import icon from '../keyboard/icon/arrow.png'
 export default class QrReaderPage extends Component {
 
-    state = {}
-
+    state = {hasDifficulty: false}
+    
     handleScan(data) {
         console.log(data)
         if (!data) return false;
@@ -17,6 +17,15 @@ export default class QrReaderPage extends Component {
     }
 
     render() {
+        if(this.state.hasDifficulty){
+            var data = {
+                msg: 'Diminua a opacidade da tela do celular', 
+                class: "message warning"
+            } 
+        } else {
+            var data = { msg: 'Apresente seu QRCODE', class: "message"};
+        }
+        
         return (
             <div className="qr-content">
                 <Link to="/" className="back-btn">
@@ -28,10 +37,24 @@ export default class QrReaderPage extends Component {
                         onError={this.handleError.bind(this)}
                     />
                 </div>
-                <p className="message">Apresente seu QRCODE</p>
+                <p className={data.class}>{data.msg}</p>
             </div>
         )
     }
+    componentDidMount() {
+        this.timerID = setTimeout(
+            () => {
+                this.setState({hasDifficulty: true});
+            },
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+    
+  
 
     handleError(err){
         console.log(err)
